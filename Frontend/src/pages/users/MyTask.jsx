04d0a45a932597b805d0b864638f18d785cc16be
Vrushabh Pawar <vrushabhpawar05@gmail.com>
2026-06-21 19:54:44 +0530

    Some changes

--- Frontend/src/pages/users/MyTask.jsx
+++ Frontend/src/pages/users/MyTask.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState } from 'react';
 import api from '../../api/axios';
 import StatusBadge from '../../components/StatusBadge';
