b8a4016bc19f5a4704644953bb533e118549f96b
Vrushabh Pawar <vrushabhpawar05@gmail.com>
2026-06-21 21:13:50 +0530

    Adding the health route

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,47 +1,36 @@
-import dotenv from 'dotenv'
-dotenv.config()
-import express from 'express'
-import connectDB from './config/db.config.js'
-import { fileURLToPath } from "url";
-import path from 'path'
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import connectDB from "./config/db.config.js";
 
-import cookieParser from 'cookie-parser'
-import cors from 'cors'
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const app = express();
+const PORT = process.env.PORT;
 
-const app = express()
-const PORT = process.env.PORT
-
-app.use(cookieParser())
-app.use(cors({
-    origin : process.env.CLIENT,
-    credentials : true
-}))
-app.use(express.json())
+app.use(cookieParser());
 app.use(
-  express.static(
-    path.join(__dirname, "../Frontend/dist")
-  )
+  cors({
+    origin: [process.env.CLIENT, "http://localhost:5173"],
+    credentials: true,
+  }),
 );
+app.use(express.json());
 
-console.log(__dirname, "../../Frontend/dist/index.html")
-await connectDB()
+await connectDB();
 app.listen(PORT, async (req, res) => {
-    console.log(`Server : http://localhost:${PORT}`)
-})
-app.get("*", (req, res) => {
-  res.sendFile(
-    path.join(__dirname, "../Frontend/dist/index.html")
-  );
+  console.log(`Server : http://localhost:${PORT}`);
 });
 
-import authRoute from './routes/auth.routes.js'
-import taskRoute from './routes/task.routes.js'
-import boardRoute from './routes/board.routes.js'
+app.get('/', (req, res) => {
+  res.send('Server Running')
+})
 
-app.use('/api/board', boardRoute)
-app.use('/api/task', taskRoute)
-app.use('/api/auth', authRoute)
+import authRoute from "./routes/auth.routes.js";
+import taskRoute from "./routes/task.routes.js";
+import boardRoute from "./routes/board.routes.js";
 
+app.use("/api/board", boardRoute);
+app.use("/api/task", taskRoute);
+app.use("/api/auth", authRoute);
